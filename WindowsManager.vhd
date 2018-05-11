41957a4a8c9159ba73c2eead836906beb958e20c
ivanvlizarazo <34040633+ivanvlizarazo@users.noreply.github.com>
2018-05-11 13:03:30 -0500

    Add files via upload

--- WindowsManager.vhd
+++ WindowsManager.vhd
@@ -1,36 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    16:31:05 05/02/2018 
--- Design Name: 
--- Module Name:    WindowsManager - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.all;
-use IEEE.std_logic_unsigned.all;
-
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.std_logic_arith.all;
 
 entity WindowsManager is
     Port ( rs1 : in  STD_LOGIC_VECTOR (4 downto 0);
@@ -38,96 +9,69 @@ entity WindowsManager is
            rd : in  STD_LOGIC_VECTOR (4 downto 0);
            op : in  STD_LOGIC_VECTOR (1 downto 0);
            op3 : in  STD_LOGIC_VECTOR (5 downto 0);
-           cwp : in  STD_LOGIC;
+           CWP : in  STD_LOGIC;
            nrs1 : out  STD_LOGIC_VECTOR (5 downto 0);
-           nrs2 : out  STD_LOGIC_VECTOR (5 downto 0);
+           nrs2 : out STD_LOGIC_VECTOR (5 downto 0);
            nrd : out  STD_LOGIC_VECTOR (5 downto 0);
-           ncwp : out  STD_LOGIC);
+			  ncwp : out STD_LOGIC);
 end WindowsManager;
 
 architecture Behavioral of WindowsManager is
-
 begin
+	process(rs1, rs2, rd, op, op3, CWP)
+	begin
+		if op = "10" then
+			if (op3 = "111100")  then -- SAVE
+				ncwp <= '0';
+			elsif (op3 = "111101") then -- RESTORE
+				ncwp <= '1';
+			else
+				ncwp <= CWP;
+			end if;
+			
+		end if;
+		
+		
+		if ( rs1>= "11000" and rs1 <= "11111") then
+			nrs1 <=conv_std_logic_vector(conv_integer(rs1) - conv_integer(CWP)* 16, 6) ;
+		elsif (rs1>= "10000" and rs1 <= "10111") then
+			nrs1 <= conv_std_logic_vector(conv_integer(rs1) + conv_integer(CWP) * 16,6) ;
+		elsif ( rs1>= "01000" and rs1 <= "01111") then
+			nrs1 <= conv_std_logic_vector(conv_integer(rs1) + conv_integer(CWP) * 16,6) ;
+		else 
+			nrs1(4 downto 0) <=  rs1;
+			nrs1 (5) <= '0';
+		end if;
+		
+		if ( rs2>= "11000" and rs2 <= "11111") then
+			nrs2 <=conv_std_logic_vector(conv_integer(rs2) - conv_integer(CWP)* 16, 6) ;
+		elsif (rs2>= "10000" and rs2 <= "10111") then
+			nrs2 <= conv_std_logic_vector(conv_integer(rs2) + conv_integer(CWP) * 16,6) ;
+		elsif ( rs2>= "01000" and rs2 <= "01111") then
+			nrs2 <= conv_std_logic_vector(conv_integer(rs2) + conv_integer(CWP) * 16,6) ;
+		else 
+			nrs2(4 downto 0) <=  rs2;
+			nrs2 (5) <= '0';
+		end if;
+		
+		if (((op3 = "111101") and op = "10") or (CWP = '1' and op3 /= "111100")) then
+			if ( rd>= "11000" and rd <= "11111") then
+				nrd <=conv_std_logic_vector(conv_integer(rd) - conv_integer('1')* 16, 6) ;
+			elsif (rd>= "10000" and rd <= "10111") then
+				nrd <= conv_std_logic_vector(conv_integer(rd) + conv_integer('1') * 16,6) ;
+			elsif ( rd>= "01000" and rd <= "01111") then
+				nrd <= conv_std_logic_vector(conv_integer(rd) + conv_integer('1') * 16,6) ;
+			else 
+				nrd(4 downto 0) <=  rd;
+				nrd (5) <= '0';
+			end if;
+		else
+			nrd(4 downto 0) <=  rd;
+			nrd (5) <= '0';
+		end if;
+		
+		
 
-	process(rs1, rs2, rd, op, op3, cwp)
-		begin
-		
-		
-			-------------SAVE AND RESTORE -----------------------
-			if(op = "10") then
-				if (op3 = "111100") then ---SAVE
-					ncwp <= '0';
-				elsif (op3 = "111101") then --RESTORE
-					ncwp <= '1';
-				end if;
-			end if;
-		
-			------- RS1 ----------------------------------
-			----- out , local ------------
-			if(conv_integer(rs1) >= 8) and (conv_integer(rs1) <= 23) then
-				
-				if(cwp = '1') then
-					nrs1 <= ('0' & rs1) + "010000";
-				elsif (cwp = '0') then
-					nrs1 <= ('0' & rs1);
-				end if;
-			---- globales-------	
-			elsif(conv_integer(rs1) <= 7 ) then 
-				nrs1 <= ('0' & rs1);
-				else 
-				------- i (entrada) ---------------
-					if(cwp = '1') then
-						nrs1 <= ('0' & rs1) - "010000";
-						elsif (cwp = '0') then
-						nrs1 <= ('0' & rs1);
-					end if; 
-			end if;
-			
-			------- RS2 ----------------------------------
-			if(conv_integer(rs2) >= 8) and (conv_integer(rs2) <= 23) then
-				
-				if(cwp = '1') then
-					nrs2 <= ('0' & rs2) + "010000";
-				elsif (cwp = '0') then
-					nrs2 <= ('0' & rs2);
-				end if;
-			
-			elsif(conv_integer(rs2) <= 7 ) then 
-				nrs2 <= ('0' & rs2);
-				else
-					if(cwp = '1') then
-						nrs2 <= ('0' & rs2) - "010000";
-						elsif (cwp = '0') then
-						nrs2 <= ('0' & rs2);
-					end if;	
-			end if;
-			
-			
-
-			
-			------- RD ----------------------------------
-			
-			if(conv_integer(rd) >= 8) and (conv_integer(rd) <= 23) then
-				
-				if(cwp = '1') then
-					nrd <= ('0' & rd) + "010000";
-				elsif (cwp = '0') then
-					nrd <= ('0' & rd);
-				end if;
-			
-			elsif(conv_integer(rd) <= 7 ) then 
-				nrd <= ('0' & rd);			
-				else 
-					if(cwp = '1') then
-						nrd <= ('0' & rd) - "010000";
-						elsif (cwp = '0') then
-						nrd <= ('0' & rd);
-					end if;
-			end if;
-			
-			
 	end process;
 
-
 end Behavioral;
-
